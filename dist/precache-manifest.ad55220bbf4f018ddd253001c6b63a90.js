87455b33f82de1bad71d26aed595d40fc26223fc
waltermolina <waltermolina@msn.com>
2018-10-04 04:53:47 -0300

    Added dist folder (excluded from .gitignore)

---
+++ dist/precache-manifest.ad55220bbf4f018ddd253001c6b63a90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1aad1d4395e6a5332a0",
+    "url": "/css/chunk-vendors.b3d8020a.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "42cd407ac295bb16bb57172319c35b15",
+    "url": "/media/animated gradient.42cd407a.mp4"
+  },
+  {
+    "revision": "a1aad1d4395e6a5332a0",
+    "url": "/js/chunk-vendors.8f99bb97.js"
+  },
+  {
+    "revision": "0ae80de59bb5e1673a9c",
+    "url": "/js/chunk-4316af3c.6fb87239.js"
+  },
+  {
+    "revision": "fa4553528db25704f4fc",
+    "url": "/js/chunk-10d2eaa0.9d37e306.js"
+  },
+  {
+    "revision": "2fbd274f0eddae305b57",
+    "url": "/js/app.3e917a78.js"
+  },
+  {
+    "revision": "d2bbadd00c054b3dd6ea0e950d2679a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d997f940feb1c15223c356534f835a2e",
+    "url": "/img/logo.d997f940.jpg"
+  },
+  {
+    "revision": "0ae80de59bb5e1673a9c",
+    "url": "/css/chunk-4316af3c.0e8250fc.css"
+  },
+  {
+    "revision": "fa4553528db25704f4fc",
+    "url": "/css/chunk-10d2eaa0.9d717fcc.css"
+  },
+  {
+    "revision": "2fbd274f0eddae305b57",
+    "url": "/css/app.dcac3a21.css"
+  }
+];
